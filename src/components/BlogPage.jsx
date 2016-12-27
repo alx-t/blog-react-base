9304ff4c54b823092b5daf5d687ceaf160c0770e
Alex L. Tesevich <alex.tesevich@gmail.com>
2016-12-27 11:08:16 +0300

    lesson 03 task 1 r.1

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bind, map } from 'lodash';
 
 import BlogList from './BlogList';
 import { items } from '../constants/items'
@@ -7,12 +8,24 @@ export default class BlogPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = { items: items };
+
+    this.incrementLikes = bind(this.incrementLikes,this);
+  }
+
+  incrementLikes(itemId) {
+    const newItems =  map(this.state.items, function(item) {
+      if (item.id === itemId) {
+        ++item.likes;
+      }
+      return item;
+    })
+    this.setState({ items: newItems });
   }
 
   render() {
     return (
       <div>
-        <BlogList items={this.state.items} />
+        <BlogList items={this.state.items} likeHandler={this.incrementLikes} />
       </div>
     )
   }
--- src/constants/items.js
+++ src/constants/items.js
@@ -1,5 +1,6 @@
 export const items = [
   {
+    id: 1,
     image: {
       alt: 'cat image',
       src: 'https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg',
@@ -15,9 +16,10 @@ export const items = [
     likes: 5
   },
   {
+    id: 2,
     image: {
       alt: 'cat image',
-      src: 'https://yt3.ggpht.com/-V92UP8yaNyQ/AAAAAAAAAAI/AAAAAAAAAAA/zOYDMx8Qk3c/s900-c-k-no-mo-rj-c0xffffff/photo.jpg',
+      src: 'http://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat.jpg',
       width: 60,
       height: 46
     },
@@ -30,6 +32,7 @@ export const items = [
     likes: 1
   },
   {
+    id: 3,
     image: {
       alt: 'cat image',
       src: 'https://s-media-cache-ak0.pinimg.com/736x/af/f5/a5/aff5a5083067a808dc45e5c5836a38c3.jpg',
