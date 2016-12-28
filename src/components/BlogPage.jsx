f83b70403ce5b841bbe4367e5bb86eb225933cb3
Alex L. Tesevich <alex.tesevich@gmail.com>
2016-12-28 11:23:50 +0300

    lesson 03 task 1 r.3

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bind, map, assign } from 'lodash';
+import { bind, map, cloneDeep } from 'lodash';
 
 import BlogList from './BlogList';
 import PieChart from './PieChart';
@@ -13,9 +13,7 @@ export default class BlogPage extends React.Component {
   }
 
   incrementLikes(itemId) {
-    const items = assign({}, this.state.items);
-    //const newItems =  map(this.state.items, function(item) {
-    const newItems =  map(items, function(item) {
+    const newItems =  map(cloneDeep(this.state.items), function(item) {
       if (item.id === itemId) ++item.likes;
       return item;
     })
